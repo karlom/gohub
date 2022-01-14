437aa9924053cea40f387f6fb23aaa6e46bec975
b65_100004238592_chuang <b65_100004238592_chuang@git.cloud.tencent.com>
2022-01-14 18:00:01 +0800

    增加一个版本接口

--- routes/api.go
+++ routes/api.go
@@ -11,7 +11,16 @@ func RegisterAPIRoute(r *gin.Engine) {
 	{
 		v1.GET("/", func(c *gin.Context) {
 			c.JSON(http.StatusOK, gin.H{
-				"Hello": "world",
+				"Hello": "world at v1",
+			})
+		})
+	}
+
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"Hello": "world at v2",
 			})
 		})
 	}
