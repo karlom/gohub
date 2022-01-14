797bd9ebe84da84d488a51a7e8c22a0ec6bcd030
b65_100004238592_chuang <b65_100004238592_chuang@git.cloud.tencent.com>
2022-01-14 17:40:13 +0800

    配置方案实现

--- main.go
+++ main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gohub/bootstrap"
+	btsConig "gohub/config"
+	"gohub/pkg/config"
 
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	// 加载 config 目录下的配置信息
+	btsConig.Initialize()
+}
+
 func main() {
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
+
 	router := gin.New()
 	bootstrap.SetupRoute(router)
 
-	err := router.Run(":3000")
+	err := router.Run(":" + config.Get("app.port"))
 
 	if err != nil {
 		fmt.Println(err.Error())
