15cbcd996419844adc200782b096250f6a9ccf57
b65_100004238592_chuang <b65_100004238592_chuang@git.cloud.tencent.com>
2022-01-22 17:26:07 +0800

    手机是否注册接口

--- routes/api.go
+++ routes/api.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"gohub/app/http/controllers/api/v1/auth"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -9,6 +10,13 @@ import (
 func RegisterAPIRoute(r *gin.Engine) {
 	v1 := r.Group("/v1")
 	{
+		authGroup := v1.Group("/auth")
+		{
+			suc := new(auth.SignupController)
+			// 判断手机是否已注册
+			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+		}
+
 		v1.GET("/", func(c *gin.Context) {
 			c.JSON(http.StatusOK, gin.H{
 				"Hello": "world at v1",
