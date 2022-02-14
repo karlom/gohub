771a8c36e4a5473354ef1aa6ba1068b7067e6e56
b65_100004238592_chuang <b65_100004238592_chuang@git.cloud.tencent.com>
2022-02-14 10:43:25 +0800

    response 包

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -5,6 +5,7 @@ import (
 	v1 "gohub/app/http/controllers/api/v1"
 	"gohub/app/models/user"
 	"gohub/app/requests"
+	"gohub/pkg/response"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -28,7 +29,7 @@ func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 	}
 
 	//  检查数据库并返回响应
-	c.JSON(http.StatusOK, gin.H{
+	response.JSON(c, gin.H{
 		"exist": user.IsPhoneExist(request.Phone),
 	})
 }
@@ -44,7 +45,7 @@ func (sc *SignupController) IsEmailExist(c *gin.Context) {
 	}
 
 	//  检查数据库并返回响应
-	c.JSON(http.StatusOK, gin.H{
+	response.JSON(c, gin.H{
 		"exist": user.IsEmailExist(request.Email),
 	})
 }
@@ -52,34 +53,9 @@ func (sc *SignupController) IsEmailExist(c *gin.Context) {
 // 抽奖，返回奖品ID
 func (sc *SignupController) Lottery(c *gin.Context) {
 
-	// 初始化请求对象
-	// request := c.Request
-
-	// // 解析 JSON 请求
-	// if err := c.ShouldBindJSON(&request); err != nil {
-	// 	// 解析失败，返回 422 状态码和错误信息
-	// 	c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-	// 		"error": err.Error(),
-	// 	})
-	// 	// 打印错误信息
-	// 	fmt.Println(err.Error())
-	// 	// 出错了，中断请求
-	// 	return
-	// }
-
-	// 表单验证
-	// errs := requests.ValidateSignupEmailExist(&request, c)
-	// errs 返回长度等于零即通过，大于 0 即有错误发生
-	// if len(errs) > 0 {
-	// 	// 验证失败，返回 422 状态码和错误信息
-	// 	c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-	// 		"errors": errs,
-	// 	})
-	// 	return
-	// }
-
 	//  检查数据库并返回响应
 	c.JSON(http.StatusOK, gin.H{
 		"这次抽奖奖品ID是": user.IsLottery(),
 	})
+
 }
