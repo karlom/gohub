ad0e7fbd85efe8fbb0a2f9f9c8b96f9aa2d126c7
b65_100004238592_chuang <b65_100004238592_chuang@git.cloud.tencent.com>
2022-02-14 11:52:03 +0800

    Email + 邮件验证码注册

--- routes/api.go
+++ routes/api.go
@@ -18,6 +18,7 @@ func RegisterAPIRoute(r *gin.Engine) {
 			// 判断 Email 是否已注册
 			authGroup.POST("/signup/email/exist", suc.IsEmailExist)
 			authGroup.POST("/signup/using-phone", suc.SignupUsingPhone)
+			authGroup.POST("/signup/using-email", suc.SignupUsingEmail)
 
 			// 发送验证码
 			vcc := new(auth.VerifyCodeController)
