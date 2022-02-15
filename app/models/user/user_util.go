aa792c1a9006cdc3e8f97aadc1301720a3b124da
b65_100004238592_chuang <b65_100004238592_chuang@git.cloud.tencent.com>
2022-02-15 18:05:09 +0800

    手机 + 短信验证码登录

--- app/models/user/user_util.go
+++ app/models/user/user_util.go
@@ -54,3 +54,19 @@ func IsLottery() int {
 
 	return lotteryNum
 }
+
+// GetByPhone 通过手机号来获取用户
+func GetByPhone(phone string) (userModel User) {
+	database.DB.Where("phone = ?", phone).First(&userModel)
+	return
+}
+
+// GetByMulti 通过 手机号/Email/用户名 来获取用户
+func GetByMulti(loginID string) (userModel User) {
+	database.DB.
+		Where("phone = ?", loginID).
+		Or("email = ?", loginID).
+		Or("name = ?", loginID).
+		First(&userModel)
+	return
+}
