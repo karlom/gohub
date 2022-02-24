f88b4f5a8f9319dd7e7fd89510325edc5107b1e6
b65_100004238592_chuang <b65_100004238592_chuang@git.cloud.tencent.com>
2022-02-24 16:24:05 +0800

    用户列表分页

--- app/models/user/user_util.go
+++ app/models/user/user_util.go
@@ -2,8 +2,12 @@ package user
 
 import (
 	"fmt"
+	"gohub/pkg/app"
 	"gohub/pkg/database"
+	"gohub/pkg/paginator"
 	"math/rand"
+
+	"github.com/gin-gonic/gin"
 )
 
 // IsEmailExist 判断 Email 已被注册
@@ -85,5 +89,17 @@ func GetByEmail(email string) (userModel User) {
 // All 获取所有用户数据
 func All() (users []User) {
 	database.DB.Find(&users)
+	return
+}
+
+// Paginate 分页内容
+func Paginate(c *gin.Context, perPage int) (users []User, paging paginator.Paging) {
+	paging = paginator.Paginate(
+		c,
+		database.DB.Model(User{}),
+		&users,
+		app.V1URL(database.TableName(&User{})),
+		perPage,
+	)
 	return
 }
