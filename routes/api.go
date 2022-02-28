84044299aa70ab5bfd3e1e94e56f283f9ec814ba
b65_100004238592_chuang <b65_100004238592_chuang@git.cloud.tencent.com>
2022-02-28 14:25:51 +0800

    显示话题

--- routes/api.go
+++ routes/api.go
@@ -77,6 +77,7 @@ func RegisterAPIRoute(r *gin.Engine) {
 			tpcGroup.POST("", middlewares.AuthJWT(), tpc.Store)
 			tpcGroup.PUT("/:id", middlewares.AuthJWT(), tpc.Update)
 			tpcGroup.DELETE("/:id", middlewares.AuthJWT(), tpc.Delete)
+			tpcGroup.GET("/:id", tpc.Show)
 		}
 		v1.GET("/", func(c *gin.Context) {
 			c.JSON(http.StatusOK, gin.H{
